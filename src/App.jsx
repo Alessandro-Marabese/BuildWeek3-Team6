7472be9dc5a3ee54bfabb6fa5b06257364b82cb4
lorenzocara22 <caralorenzo2200@gmail.com>
2025-02-24 17:57:11 +0100

    SuggestedPeople mod

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import SettingsPage from "./components/Settings/SettingsPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CardProfile from "./components/CardProfile";
 import "./App.css";
+import SuggestedPeople from "./components/SuggestedPeople";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<CardProfile />} />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <CardProfile />
+              <SuggestedPeople />
+            </>
+          }
+        />
+
         <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
