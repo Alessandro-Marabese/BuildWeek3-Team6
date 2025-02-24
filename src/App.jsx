e1191e2d8d51689c706d34d8706c36320ed81e05
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-02-24 16:35:04 +0000

    ok merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import SettingsPage from "./components/Settings/SettingsPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CardProfile from "./components/CardProfile";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <CardProfile />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<CardProfile />} />
         <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
