b07e9e82d84c2e9153b6ca9fe9d5bfcb1b9fc118
harshpreet singh <harshsingh.hs.389.hs@gmail.com>
2025-02-24 17:17:07 +0100

    settings2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage"; 
 import SettingsPage from "./components/Settings/SettingsPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
+    <BrowserRouter> 
     <Routes>
       <Route path="/" element={<HomePage />} /> 
       <Route path="/settings" element={<SettingsPage />} /> 
     </Routes>
+    </BrowserRouter> 
   );
 }
 
