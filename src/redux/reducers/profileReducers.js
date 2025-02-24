28d745f8b021b84aa3fcd05707d24b37cfc51477
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-02-24 16:07:29 +0000

    fetch profile

--- src/redux/reducers/profileReducers.js
+++ src/redux/reducers/profileReducers.js
@@ -1,9 +1,16 @@
+import { FETCH_PROFILE_OK } from "../actions";
+
 const initialState = {
-  content: null,
+  content: [],
 };
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_PROFILE_OK:
+      return {
+        ...state,
+        content: action.payload,
+      };
     default:
       return state;
   }
