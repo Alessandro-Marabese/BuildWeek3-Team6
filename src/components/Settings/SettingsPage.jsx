bf3c8ab5f186c5061c2c6e1e03ff8ebf0430a948
harshpreet singh <harshsingh.hs.389.hs@gmail.com>
2025-02-25 15:02:26 +0100

    color

--- src/components/Settings/SettingsPage.jsx
+++ src/components/Settings/SettingsPage.jsx
@@ -5,7 +5,7 @@ import "./Settings.css";
 
 const SettingsPage = () => {
   return (
-    <div style={{ width: "100%", padding: "20px" }}>
+    <div style={{ width: "100%", padding: "20px", backgroundColor: "white" }}>
       <SettingsHeader />
       <hr style={{ border: "1px solid #e0e0e0", margin: "10px 0" }} />
       <SettingsList />
