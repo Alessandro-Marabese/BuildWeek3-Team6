1afafb78d29bfa0de34ef2fd021b2277a7ff6c47
LT-LuciaT <lucia.trombin98@gmail.com>
2025-02-27 09:42:12 +0100

    icona +

--- src/components/Esperienza.jsx
+++ src/components/Esperienza.jsx
@@ -1,13 +1,7 @@
 import { useState, useEffect } from "react";
 import { Col, Container, Row, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchExperiences,
-  //addExperience,
-  // updateExperience,
-  deleteExperience,
-  /*   uploadExperienceImage, */
-} from "../redux/actions/index";
+import { fetchExperiences, deleteExperience } from "../redux/actions/index";
 import ExperienceModal from "./ExperienceModal";
 
 const Esperienza = () => {
@@ -33,19 +27,6 @@ const Esperienza = () => {
     setExperienceToEdit(exp);
     setShowModal(true);
   };
-
-  // const handleSubmit = (formData) => {
-  //   if (!userId) {
-  //     alert("User ID non trovato!");
-  //     return;
-  //   }
-
-  //   if (experienceToEdit) {
-  //     dispatch(updateExperience(userId, experienceToEdit._id, formData));
-  //   } else {
-  //     dispatch(addExperience(userId, formData));
-  //   }
-  // };
 
   const handleDelete = (experienceId) => {
     dispatch(deleteExperience(userId, experienceId));
@@ -107,21 +88,32 @@ const Esperienza = () => {
               <p className="text-black-50">{exp.area}</p>
               <p className="text-black-50">{exp.description}</p>
             </div>
-            <div className="icon-modifica ms-auto">
+            <div className="icon-modifica ms-auto d-flex align-items-start">
               <svg
                 viewBox="0 0 24 24"
                 data-supported-dps="24x24"
                 fill="currentColor"
-                className="icon i24x24"
-                style={{ width: "24px" }}
+                className="icon i24x24 me-2"
+                style={{ width: "24px", cursor: "pointer" }}
                 onClick={() => handleEditClick(exp)}
               >
                 <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
               </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                className="icon i24x24 d-none d-md-block"
+                style={{ width: "24px", cursor: "pointer" }}
+                onClick={handleAddClick}
+              >
+                <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
+              </svg>
             </div>
           </div>
         ))}
-        <Row>
+        <Row className="d-md-none">
           <h5>Hai altra esperienza?</h5>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequuntur velit quae, eius temporibus
--- src/components/Formazione.jsx
+++ src/components/Formazione.jsx
@@ -4,7 +4,7 @@ import { Col, Container, Row, Button } from "react-bootstrap";
 const Formazione = () => {
   return (
     <Col className="ps-lg-3">
-      <Container fluid className="mx-0  mt-2 cont  pt-3 rounded-block">
+      <Container fluid className="mx-0 mt-2 cont pt-3 rounded-block">
         <h5>Formazione</h5>
         <div className="d-flex">
           <div>
@@ -19,32 +19,42 @@ const Formazione = () => {
             <div className="d-flex">
               <div className="text-center">Istituzione</div>
               <div className="dot-separator"></div>
-              <div className="text-center"> Titolo </div>
+              <div className="text-center">Titolo</div>
             </div>
             <div>
-              <div className="text-black-50"> Inzio e fine </div>
+              <div className="text-black-50">Inizio e fine</div>
             </div>
           </div>
-          <div className="icon-modifica">
+          <div className="icon-modifica ms-auto d-flex align-items-start">
             <svg
               viewBox="0 0 24 24"
               data-supported-dps="24x24"
               fill="currentColor"
-              className="icon i24x24"
-              style={{ width: "24px" }}
+              className="icon i24x24 me-2"
+              style={{ width: "24px", cursor: "pointer" }}
             >
               <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
             </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              data-supported-dps="24x24"
+              fill="currentColor"
+              className="icon i24x24 d-none d-md-block"
+              style={{ width: "24px", cursor: "pointer" }}
+            >
+              <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
+            </svg>
           </div>
         </div>
-        <Row>
+        <Row className="d-md-none">
           <h5>Hai altri titoli di studio?</h5>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequuntur velit quae, eius temporibus
             voluptate! Rerum placeat qui quo ratione labore rem at! Doloremque consequatur sequi iste eum earum.
             Placeat.
           </p>
-          <Button variant="outline-primary"> Aggiungi titolo di studio </Button>
+          <Button variant="outline-primary">Aggiungi titolo di studio</Button>
         </Row>
       </Container>
     </Col>
